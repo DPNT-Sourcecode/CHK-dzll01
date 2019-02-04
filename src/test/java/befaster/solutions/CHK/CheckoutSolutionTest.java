ab777e298b3fcc6e330d407ecaed929ab6319656
sbx_user1060 <sbx_user1060@ip-10-129-153-251.eu-west-2.compute.internal>
2019-02-04 05:06:06 +0000

    Mon Feb 04 05:06:06 UTC 2019

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,19 +1,49 @@
 package befaster.solutions.CHK;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class CheckoutSolutionTest {
+	
+	private CheckoutSolution checkout;
 
 	@Before
 	public void setUp() throws Exception {
+		checkout = new CheckoutSolution();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testA() {
+		assertThat(checkout.checkout("A"), equalTo(50));
+	}
+	@Test
+	public void testB() {
+		assertThat(checkout.checkout("B"), equalTo(30));
+	}
+	
+	@Test
+	public void testC() {
+		assertThat(checkout.checkout("C"), equalTo(20));
+	}
+	
+	@Test
+	public void testD() {
+		assertThat(checkout.checkout("D"), equalTo(10));
+	}
+	
+	@Test
+	public void testOthersExample1() {
+		assertThat(checkout.checkout("T"), equalTo(-1));
+	}
+	
+	@Test
+	public void testOthersExample2() {
+		assertThat(checkout.checkout("Apple"), equalTo(-1));
 	}
 
 }
+
